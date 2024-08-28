8087a33fc007b34673818ab70eb6128179082601
manasabsv26 <manasabellamkonda7@gmail.com>
2024-08-27 18:16:30 -0700

    Adjacency List using Hash Map

--- Graph-AdjacencyList/main.cpp
+++ Graph-AdjacencyList/main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <list>
+#include <cstring>
+#include <unordered_map>
 using namespace std;
 
 class Graph{
 
-    int V;
-    list<int> *l;
+    unordered_map<string, list< pair<string, int> > > l;
 
 public:
-    Graph(int V){
-        this->V = V;
-        l= new list<int>[V];
-    }
-
-    void addEdge(int x, int y){
-        l[x].push_back(y);
-        l[y].push_back(x);
+    //no constructor needed
+    void addEdge(string x, string y, bool bidir, int wt){
+        l[x].push_back(make_pair(y, wt));
+        if(bidir){
+            l[y].push_back(make_pair(x,wt));
+        }
     }
     void printAdjList(){
-        for(int i=0;i<V;i++){
-            cout<<"Vertex"<<i<<": ";
-            for(int x:l[i]){
-                cout<<x<<",";
+        for( auto item:l){
+            //item is a key value pair where key is the source city, and value is list of pairs of dest. cities and wts, [(a,20),(b,50), ...]
+            string source = item.first; //key
+            list<pair<string, int>> destinations = item.second;
+            cout<<source<<": ";
+            for(auto destination:destinations){
+                cout<<"( "<<destination.first<<", "<<destination.second<<")"<<",";
             }
             cout<<endl;
         }
@@ -30,11 +32,12 @@ public:
 
 int main()
 {
-    Graph g(4);
-    g.addEdge(0,1);
-    g.addEdge(0,2);
-    g.addEdge(2,3);
-    g.addEdge(1,2);
+    Graph g;
+    g.addEdge("A","B", true, 20);
+    g.addEdge("B","D", true, 30);
+    g.addEdge("C","D", true, 40);
+    g.addEdge("A","C", true, 10);
+    g.addEdge("A","D", false, 50);
     g.printAdjList();
 
     return 0;
